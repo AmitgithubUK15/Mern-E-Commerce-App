a32c50cba29915b6852816e8d550dfedbfd6bc26
Amit <Amitshah050112003@gmail.com>
2024-03-05 18:01:48 +0530

    connect to database

--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
 
-
+mongoose.connect(process.env.MONGODB)
+.then(()=>{
+    console.log("mongoDB connected successfully")
+})
+.catch((err)=>{
+    console.log(err);
+})
 
 app.listen(8005,()=>{
     console.log("server is started at port 8005");
