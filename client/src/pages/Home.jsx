4af4e7096b408cbbbb3d58b031c2cce95d7d2b4a
Amit <Amitshah050112003@gmail.com>
2024-03-05 17:48:10 +0530

    create and run server

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+
+import UserBar from '../components/UserBar'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>Home
+      {/* <UserBar /> */}
+    </div>
   )
 }
---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+
+
+app.listen(8005,()=>{
+    console.log("server is started at port 8005");
+})
